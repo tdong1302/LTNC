a3e6cfa8c5511747c88d1ac0c0272fd3701fc60c
tdong1302 <159911414+tdong1302@users.noreply.github.com>
2024-03-03 01:35:28 +0700

    B21.cpp

--- BT01/B21.cpp
+++ BT01/B21.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(){
-    int x,y,z;
-    cin>>x>>y>>z;
-    bool b;
-    if((x<y&&y<z)||(x>y&&y>z)){
-        b=true;
+int main() {
+    double a,b,c;
+    cin>>a>>b>>c;
+    if (a+b>c&&a+c>b&&b+c>a) {
+        double s=(a+b+c)/2;
+        double area =sqrt(s*(s-a)*(s-b)*(s-c));
+        cout<<area;
+    } else {
+        cout<<"k phải tam giác";
     }
-    else b=false;
-    cout<<b;
 }
-
-
