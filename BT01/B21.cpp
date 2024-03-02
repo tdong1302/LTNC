1128b99b005a3a1d00e68813bd1790dbb831bcef
tdong1302 <dongtv1302@gmail.com>
2024-03-03 00:00:07 +0700

    .

---
+++ BT01/B21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int x,y,z;
+    cin>>x>>y>>z;
+    bool b;
+    if((x<y&&y<z)||(x>y&&y>z)){
+        b=true;
+    }
+    else b=false;
+    cout<<b;
+}
+
+
